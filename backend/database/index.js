48248566b33353363d2960baf5d5bd72675f907d
Lawi Salim <dahlawibrahim@gmail.com>
2025-08-17 22:30:58 +0000

    fix: Use DATABASE_URL for production

--- backend/database/index.js
+++ backend/database/index.js
@@ -6,19 +6,17 @@ const env = process.env.NODE_ENV || 'development';
 const dbConfig = config[env];
 
 // Initialisation de la connexion Sequelize
-const sequelize = new Sequelize(
-  dbConfig.database,
-  dbConfig.username,
-  dbConfig.password,
-  {
-    host: dbConfig.host,
-    port: dbConfig.port,
-    dialect: dbConfig.dialect,
-    logging: dbConfig.logging,
-    pool: dbConfig.pool,
-    dialectOptions: dbConfig.dialectOptions
-  }
-);
+let sequelize;
+if (dbConfig.use_env_variable) {
+  sequelize = new Sequelize(process.env[dbConfig.use_env_variable], dbConfig);
+} else {
+  sequelize = new Sequelize(
+    dbConfig.database,
+    dbConfig.username,
+    dbConfig.password,
+    dbConfig
+  );
+}
 
 module.exports = {
   sequelize,
